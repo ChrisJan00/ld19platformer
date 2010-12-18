43edefbfe54b6bab90e557d10c469676c60538f0
ChrisJan00 <cjan_ynzn@yahoo.es>
2010-12-18 14:30:11 +0100

    Adding keyboard support

--- gamecanvas.js
+++ gamecanvas.js
@@ -1,20 +1,63 @@
+// ---------------------------------------------------------
+// GLOBAL OBJECTS
+
+// timer engine
 var gameControl = new( function() {
-        // timer engine
-	this.fps = 60;
-	this.startTime = new Date().getTime();
-	this.stopTime = this.startTime;
-	this.elapsed = 0;
-	this.dt = 0;
-	this.step = 1000/this.fps;
-	this.skip = false;
+    this.fps = 60;
+    this.startTime = new Date().getTime();
+    this.stopTime = this.startTime;
+    this.elapsed = 0;
+    this.dt = 0;
+    this.step = 1000/this.fps;
+    this.skip = false;
 } )
 
+// keyboard input
+var keys = new( function() {
+    this.upPressed = false;
+    this.downPressed = false;
+    this.leftPressed = false;
+    this.rightPressed = false;
+    this.upCode = 38;
+    this.downCode = 40;
+    this.leftCode = 37;
+    this.rightCode = 39;
+    this.keyPressed = function(event) {
+	switch (event.keyCode) {
+	    case keys.upCode:  keys.upPressed = true;
+	    break;
+	    case keys.downCode: keys.downPressed = true;
+	    break;
+	    case keys.leftCode: keys.leftPressed = true;
+	    break;
+	    case keys.rightCode: keys.rightPressed = true;
+	    break;
+	}
+    }
+    this.keyReleased = function(event) {
+	switch (event.keyCode) {
+	    case keys.upCode:  keys.upPressed = false;
+	    break;
+	    case keys.downCode: keys.downPressed = false;
+	    break;
+	    case keys.leftCode: keys.leftPressed = false;
+	    break;
+	    case keys.rightCode: keys.rightPressed = false;
+	    break;
+	}
+    }
+})
+
+// ------------------------------------------------------------------------
+// CONTROL FUNCTIONS
 function startGame() {
     loadGame();
     gameControl.runInterval = setInterval(mainLoop, 1000/gameControl.fps);
 }
 
 function loadGame() {
+    document.onkeydown = keys.keyPressed;
+    document.onkeyup = keys.keyReleased;
 }
 
 function mainLoop() {
@@ -41,7 +84,7 @@ function mainLoop() {
 }
 
 //---------------------------------------
-
+// GAME LOGIC + GAME GRAPHICS
 function update(dt) {
 }
 
@@ -49,5 +92,9 @@ function draw(dt) {
     var canvas = document.getElementById("canvas1");
     var context = canvas.getContext("2d");
     context.fillStyle = "#FF00AA";
+    if (keys.upPressed) context.fillStyle = "#AAAA00";
+    if (keys.downPressed) context.fillStyle = "#00FFFF";
+    if (keys.leftPressed) context.fillStyle = "#3300DD";
+    if (keys.rightPressed) context.fillStyle = "#444444";
     context.fillRect(50, 25, 150, 100);
 }

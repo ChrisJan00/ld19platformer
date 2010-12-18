830192ce9490fc67fe82d73d3516cc1533fc95f7
ChrisJan00 <cjan_ynzn@yahoo.es>
2010-12-18 12:36:58 +0100

    first steps

---
+++ gamecanvas.js
@@ -0,0 +1,6 @@
+function draw_b() {
+  var b_canvas = document.getElementById("canvas1");
+  var b_context = b_canvas.getContext("2d");
+  b_context.fillStyle = "#FF0000";
+  b_context.fillRect(50, 25, 150, 100);
+}

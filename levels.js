864793a841729d35f26592570585953d9b3ea667
ChrisJan00 <cjan_ynzn@yahoo.es>
2011-10-16 20:12:26 +0200

    caching of level data for faster switch

--- levels.js
+++ levels.js
@@ -91,9 +91,11 @@ var Level = new( function () {
 			return true;
 	    if (levelInfo.levelImage.complete) {
 	    	if (levelInfo.levelImage.width > 0) {
-				levelInfo.exists = true;
-				levelInfo.pending = false;
-				return true;
+	    		if (_private.continueCaching(levelInfo)) {
+					levelInfo.exists = true;
+					levelInfo.pending = false;
+					return true;
+				}
 			}
 			else {
 				levelInfo.sublevelIndex = levelInfo.sublevelIndex-1;
@@ -107,6 +109,87 @@ var Level = new( function () {
 			}
 	    }
 	    return false;
+    }
+    
+    _private.continueCaching = function( levelInfo ) {
+    	if (!levelInfo.cachingStep)
+    		levelInfo.cachingStep = 0;
+    	var gW = graphics.canvasWidth;   
+	    var gH = graphics.canvasHeight;
+    	switch (levelInfo.cachingStep) {
+    		case 0: {
+    			levelInfo.bgCanvas = document.createElement("canvas");
+    			levelInfo.bgContext = levelInfo.bgCanvas.getContext("2d");
+    			levelInfo.bgCanvas.width = gW;
+    			levelInfo.bgCanvas.height = gH;
+    			levelInfo.cachingStep = 1;
+    			return false;
+    		}
+    		case 1: {
+    			levelInfo.bgContext.drawImage(levelInfo.levelImage, 0, gH*0, gW, gH, 0, 0, gW, gH);
+    			levelInfo.cachingStep = 2;
+    			return false;
+    		}
+    		case 2: {
+    			levelInfo.bgContext.drawImage(levelInfo.levelImage, 0, gH*1, gW, gH, 0, 0, gW, gH);
+    			levelInfo.cachingStep = 3;
+    			return false;
+    		}
+    		case 3: {
+    			levelInfo.bgContext.drawImage(levelInfo.levelImage, 0, gH*2, gW, gH, 0, 0, gW, gH);
+    			levelInfo.cachingStep = 4;
+    			return false;
+    		}
+    		case 4: {
+    			levelInfo.bgContext.drawImage(levelInfo.levelImage, 0, gH*3, gW, gH, 0, 0, gW, gH);
+    			levelInfo.cachingStep = 5;
+    			return false;
+    		}
+    		case 5: {
+    			levelInfo.topCanvas = document.createElement("canvas");
+    			levelInfo.topContext = levelInfo.topCanvas.getContext("2d");
+    			levelInfo.topCanvas.width = gW;
+    			levelInfo.topCanvas.height = gH;
+    			levelInfo.cachingStep = 6;
+    			return false;
+    		}
+    		case 6: {
+    			levelInfo.topContext.drawImage(levelInfo.levelImage, 0, gH*4, gW, gH, 0, 0, gW, gH);
+    			levelInfo.cachingStep = 7;
+    			return false;
+    		}
+    		case 7: {
+    			levelInfo.originalCanvas = document.createElement("canvas");
+    			levelInfo.originalContext = levelInfo.originalCanvas.getContext("2d");
+    			levelInfo.originalCanvas.width = levelInfo.levelImage.width;
+    			levelInfo.originalCanvas.height = levelInfo.levelImage.height;
+    			levelInfo.cachingStep = 8;
+    			return false;
+    		}
+    		case 8: {
+    			levelInfo.originalContext.drawImage(levelInfo.levelImage, 0, 0);
+    			levelInfo.cachingStep = 9;
+    			return false;
+    		}
+    		case 9: {
+    			levelInfo.killData = myGetImageData( levelInfo.originalContext, 0, gH * 2, gW, gH );
+    			levelInfo.cachingStep = 10;
+    			return false;
+    		}
+    		case 10: {
+    			levelInfo.wallData = myGetImageData( levelInfo.originalContext, 0, gH * 1, gW, gH );
+    			levelInfo.cachingStep = 11;
+    			return false;
+    		}
+    		case 11: {
+    			levelInfo.climbData = myGetImageData( levelInfo.originalContext, 0, gH * 3, gW, gH );
+    			levelInfo.cachingStep = 12;
+    			return false;
+    		}
+			case 12:
+				return true;
+    	}
+    	return false;
     }
 
 		
@@ -126,6 +209,17 @@ var Level = new( function () {
 		self.sublevelIndex = levelInfo.sublevelIndex;
 		graphics.getDocumentCanvas(self.levelIndex);
 		
+		graphics.bgCanvas = levelInfo.bgCanvas;
+		graphics.bgContext = levelInfo.bgContext;
+	    graphics.topCanvas = levelInfo.topCanvas;
+	    graphics.topContext = levelInfo.topContext;
+	    
+	    // generate the top layer
+	    
+	    assets.killData = levelInfo.killData;
+	    assets.wallData = levelInfo.wallData;
+	    assets.climbData = levelInfo.climbData;
+		
 		// set the start and stop positions (and velocities!)
 		// but the actual positions should be stored when the player is standing or climbing (that is, on a velocity of 0)
 		if (player.y < 0)
@@ -137,7 +231,10 @@ var Level = new( function () {
 		// but only when going up!
 		
 		// TODO: this is slow, should be done gradually during preload
-		self.loadLevel();
+		//self.loadLevel();
+		// paint the background
+	    graphics.paintBackground();
+	    self.startLevelPreloading();
 	}
 
 })
